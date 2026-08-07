e0aeacc62e70b04101baa0818e35b60b5c8ceae7
Abodh <abodhdpanchal@gmail.com>
2026-08-07 23:55:44 +0530

    Implement Pascal's Triangle generation in Java

---
+++ Dynamic Programming/0118 - Pascal's Triangle/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<>();
+        
+        for(int i = 0 ; i< numRows ; i++){
+            List<Integer> temp = new ArrayList<>();
+            for(int j = 0 ; j < i+1 ; j++){
+                if(j > 0 && j < i){
+                    temp.add(res.get(i-1).get(j-1)+res.get(i-1).get(j));
+                }
+                else temp.add(1);
+            }
+            res.add(temp);
+        }
+
+        return res;
+    }
+}
