ae7ba7a61e0342898da1f13e1f3785733b7a1d15
Abodh <abodhdpanchal@gmail.com>
2026-08-09 13:23:16 +0530

    Implement House Robber solution using dynamic programming

---
+++ Dynamic Programming/0198 - House Robber/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int rob(int[] nums) {
+        int dp [] = new int[nums.length+1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+
+        for(int i = 1 ; i < nums.length ; i++){
+            int val = nums[i];
+            dp[i+1] = Math.max(dp[i-1]+val, dp[i]);
+        }
+
+        return dp[nums.length];
+    }
+}
